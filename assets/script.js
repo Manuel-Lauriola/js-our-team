3088d2bc908ddf1b4d86ca649a4feed66c5de5e6
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-16 17:43:01 +0200

    [UPD] - create card generator function

--- assets/script.js
+++ assets/script.js
@@ -36,3 +36,22 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+//dichiaro la funzione che includa le variabili prese dai singoli oggetti in una variabile del mio html
+const generateCard = (member) => {
+  const card = `<div class="card mb-3 col-4">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${member.img}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${member.name}</h5>
+        <p class="card-text">${member.role}</p>
+        <p class="card-text"><small class="text-body-secondary">${member.email}</small></p>
+      </div>
+    </div>
+  </div>
+</div>`
+return card
+}
